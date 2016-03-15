68e32471d5ce92e21c35263ca6968c57b4a6d758
Kemal Eren <kemal@kemaleren.com>
2016-03-15 16:17:47 -0700

    implement baum-welch. refactor matrix names.

--- run.py
+++ run.py
@@ -20,27 +20,27 @@ from scipy.misc import logsumexp
 import numpy as np
 
 
-def _check_matrices(obs, startprob, transmat, emissionprob):
-    startprob = startprob.ravel()
-    n_states = transmat.shape[0]
-    n_symbols = emissionprob.shape[1]
-    if transmat.shape != (n_states, n_states):
+def _check_matrices(obs, S, A, E):
+    S = S.ravel()
+    n_states = A.shape[0]
+    n_symbols = E.shape[1]
+    if A.shape != (n_states, n_states):
         raise Exception('Transmission matrix shape {} is not'
-                        ' square.'.format(transmat.shape))
-    if len(startprob) != n_states:
+                        ' square.'.format(A.shape))
+    if len(S) != n_states:
         raise Exception('Wrong number of starting'
                         ' probabilities. Expected {} and got'
-                        ' {}.'.format(n_states, len(startprob)))
-    if emissionprob.shape != (n_states, n_symbols):
+                        ' {}.'.format(n_states, len(S)))
+    if E.shape != (n_states, n_symbols):
         raise Exception('Emission matrix has wrong number'
                         ' of states. Expected {} and got'
-                        ' {}.'.format(n_states, emissionprob.shape[0]))
+                        ' {}.'.format(n_states, E.shape[0]))
 
-    if not np.allclose(np.sum(transmat, axis=1), 1):
+    if not np.allclose(np.sum(A, axis=1), 1):
         raise Exception('Transmission probabilities do not sum to 1.')
-    if not np.allclose(startprob.sum(), 1):
+    if not np.allclose(S.sum(), 1):
         raise Exception('Starting probabilities do not sum to 1.')
-    if not np.allclose(np.sum(emissionprob, axis=1), 1):
+    if not np.allclose(np.sum(E, axis=1), 1):
         raise Exception('Emission probabilities do not sum to 1.')
 
     if np.max(obs) > n_symbols:
@@ -50,25 +50,25 @@ def _check_matrices(obs, startprob, transmat, emissionprob):
 
 
 # copied from flea_pipeline/trim.py
-def viterbi_decode(obs, startprob, transmat, emissionprob):
+def viterbi_decode(obs, S, A, E):
     """Viterbi algorithm.
 
-    transmat: (n_states x n_states) [i, j]: transition from i to j
-    emissionprob: (n_states x n_symbols)
-    startprob: (n_states,)
+    A: (n_states x n_states) [i, j]: transition from i to j
+    E: (n_states x n_symbols)
+    S: (n_states,)
 
     """
-    _check_matrices(obs, startprob, transmat, emissionprob)
-    startprob = startprob.ravel()
-    n_states = transmat.shape[0]
+    _check_matrices(obs, S, A, E)
+    S = S.ravel()
+    n_states = A.shape[0]
 
     with warnings.catch_warnings():
         # already checked probabilities, so should be safe to ignoring
         # warnings
         warnings.simplefilter("ignore")
-        logtrans = np.log(transmat)
-        logstart = np.log(startprob)
-        logemission = np.log(emissionprob)
+        logtrans = np.log(A)
+        logstart = np.log(S)
+        logemission = np.log(E)
 
     # heavily adapted from:
     # http://phvu.net/2013/12/06/sweet-implementation-of-viterbi-in-python/
@@ -91,16 +91,16 @@ def viterbi_decode(obs, startprob, transmat, emissionprob):
     return result
 
 
-def forward(obs, startprob, transmat, emissionprob):
-    _check_matrices(obs, startprob, transmat, emissionprob)
-    startprob = startprob.ravel()
-    n_states = transmat.shape[0]
+def forward(obs, S, A, E):
+    _check_matrices(obs, S, A, E)
+    S = S.ravel()
+    n_states = A.shape[0]
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        logtrans = np.log(transmat)
-        logstart = np.log(startprob)
-        logemission = np.log(emissionprob)
+        logtrans = np.log(A)
+        logstart = np.log(S)
+        logemission = np.log(E)
 
     trellis = np.zeros((n_states, len(obs)))
     trellis[:, 0] = logstart + logemission[:, obs[0]]
@@ -110,16 +110,16 @@ def forward(obs, startprob, transmat, emissionprob):
     return trellis
 
 
-def backward(obs, startprob, transmat, emissionprob):
-    _check_matrices(obs, startprob, transmat, emissionprob)
-    startprob = startprob.ravel()
-    n_states = transmat.shape[0]
+def backward(obs, S, A, E):
+    _check_matrices(obs, S, A, E)
+    S = S.ravel()
+    n_states = A.shape[0]
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        logtrans = np.log(transmat)
-        logstart = np.log(startprob)
-        logemission = np.log(emissionprob)
+        logtrans = np.log(A)
+        logstart = np.log(S)
+        logemission = np.log(E)
 
     trellis = np.zeros((n_states, len(obs)))
     trellis[:, -1] = 0
@@ -131,66 +131,98 @@ def backward(obs, startprob, transmat, emissionprob):
     return trellis
 
 
-def posterior_logprobs(obs, startprob, transmat, emissionprob):
-    f = forward(obs, startprob, transmat, emissionprob)
-    b = backward(obs, startprob, transmat, emissionprob)
+def posterior_logprobs(obs, S, A, E):
+    f = forward(obs, S, A, E)
+    b = backward(obs, S, A, E)
     logP = logsumexp(f[:, -1])
     assert np.allclose(logsumexp(f + b - logP, axis=0), 0.0)
     return f + b - logP
 
 
-def posterior_decode(obs, startprob, transmat, emissionprob):
-    p = posterior_logprobs(obs, startprob, transmat, emissionprob)
+def posterior_decode(obs, S, A, E):
+    p = posterior_logprobs(obs, S, A, E)
     return np.argmax(p, axis=0)
 
 
+def get_start(A, E):
+    vals, vecs = scipy.linalg.eig(A, left=True, right=False)
+    idx = np.argmax(vals)
+    S = vecs[:, idx]
+    S /= S.sum()
+    return S
+
+
+def baumwelch_train(observations, S, A, E):
+    while True:
+        # implicit pseudocount of 1
+        A_new = np.zeros_like(A)
+        E_new = np.zeros_like(E)
+        S_log = np.log(S)
+        A_log = np.log(A)
+        E_log = np.log(E)
+        for obs in observations:
+            f = forward(obs, S, A, E)
+            b = backward(obs, S, A, E)
+            logP = logsumexp(f[:, -1])
+            for source in range(A.shape[0]):
+                for sink in range(A.shape[1]):
+                    val = logsumexp(list(f[source, pos] + A_log[source, sink] + E_log[sink, obs[pos + 1]] + b[sink, pos + 1]
+                                         for pos in range(len(obs) - 1)))
+                    A_new[source, sink] = logsumexp([A_new[source, sink], val - logP])
+            for state in range(E.shape[0]):
+                for symbol in range(E.shape[1]):
+                    if symbol in obs:
+                        val = logsumexp(list(f[state, pos] + b[state, pos] for pos in range(len(obs)) if obs[pos] == symbol))
+                        E_new[state, symbol] = logsumexp([E_new[state, symbol], val - logP])
+        A_new = np.exp(A_new)
+        E_new = np.exp(A_new)
+        A_new = A_new / A_new.sum(axis=1).reshape((-1, 1))
+        E_new = E_new / E_new.sum(axis=1).reshape((-1, 1))
+        S = get_start(A_new, E_new)
+        if np.allclose(A_new, A) and np.allclose(E_new, E):
+            break
+        A = A_new
+        E = E_new
+    return S, A_new, E_new
+
+
 def estimate_from_paths(paths, observations, n_states, n_symbols):
-    transmat = np.zeros((n_states, n_states)) + 1  # pseudocount
-    emissionprob = np.zeros((n_states, n_symbols)) + 1  # pseudocount
+    A = np.zeros((n_states, n_states)) + 1  # pseudocount
+    E = np.zeros((n_states, n_symbols)) + 1  # pseudocount
     for path, obs in zip(paths, observations):
+        E[path[0], obs[0]] += 1
         for i in range(1, len(path)):
             source, sink = path[i - 1], path[i]
-            transmat[source, sink] += 1
-            emissionprob[sink, obs[i]] += 1
-    # normalize transmat and emissionprob
-    transmat = transmat / transmat.sum(axis=1).reshape((-1, 1))
-    emissionprob = emissionprob / emissionprob.sum(axis=1).reshape((-1, 1))
+            A[source, sink] += 1
+            E[sink, obs[i]] += 1
+    # normalize A and E
+    A = A / A.sum(axis=1).reshape((-1, 1))
+    E = E / E.sum(axis=1).reshape((-1, 1))
     # set start probabilities to equilibrium distribution
-    vals, vecs = scipy.linalg.eig(transmat, left=True, right=False)
-    idx = np.argmax(vals)
-    startprob = vecs[:, idx]
-    startprob /= startprob.sum()
-    return startprob, transmat, emissionprob
+    S = get_start(A, E)
+    return S, A, E
 
 
-def estimate_from_expectations():
-    pass
-
-
-def baumwelch_train():
-    pass
-
-
-def viterbi_train(observations, startprob, transmat, emissionprob, max_iters=100):
-    n_states = transmat.shape[0]
-    n_symbols = emissionprob.shape[1]
-    assert transmat.shape == (n_states, n_states)
-    assert emissionprob.shape == (n_states, n_symbols)
+def viterbi_train(observations, S, A, E, max_iters=100):
+    n_states = A.shape[0]
+    n_symbols = E.shape[1]
+    assert A.shape == (n_states, n_states)
+    assert E.shape == (n_states, n_symbols)
     for i in range(max_iters):
-        old_startprob = startprob.copy()
-        old_transmat = transmat.copy()
-        old_emissionprob = emissionprob.copy()
-        paths = list(viterbi_decode(o, startprob, transmat, emissionprob)
+        S_old = S.copy()
+        A_old = A.copy()
+        E_old = E.copy()
+        paths = list(viterbi_decode(o, S, A, E)
                      for o in observations)
-        startprob, transmat, emissionprob = estimate_from_paths(paths,
-                                                                observations,
-                                                                n_states,
-                                                                n_symbols)
-        if np.allclose(old_startprob, startprob) and \
-           np.allclose(old_transmat, transmat) and \
-           np.allclose(old_emissionprob, emissionprob):
+        S, A, E = estimate_from_paths(paths,
+                                      observations,
+                                      n_states,
+                                      n_symbols)
+        if np.allclose(S_old, S) and \
+           np.allclose(A_old, A) and \
+           np.allclose(E_old, E):
             break
-    return startprob, transmat, emissionprob
+    return S, A, E
 
 
 def preprocess(p1, p2, child):
@@ -208,17 +240,17 @@ def preprocess(p1, p2, child):
 
 
 def run(observation):
-    startprob = np.array([0.5, 0.5])
-    transmat = np.array([[0.9, 0.1],
-                         [0.1, 0.9]])
-    emissionprob = np.array([[0.9, 0.1],
-                             [0.1, 0.9]])
-    startprob, transmat, emissionprob = viterbi_train([observation],
-                                                      startprob,
-                                                      transmat,
-                                                      emissionprob)
-    path = posterior_decode(observation, startprob, transmat, emissionprob)
-    return path, startprob, transmat, emissionprob
+    S = np.array([0.5, 0.5])
+    A = np.array([[0.9, 0.1],
+                  [0.1, 0.9]])
+    E = np.array([[0.9, 0.1],
+                  [0.1, 0.9]])
+    S, A, E = viterbi_train([observation],
+                            S,
+                            A,
+                            E)
+    path = posterior_decode(observation, S, A, E)
+    return path, S, A, E
 
 
 if __name__ == "__main__":
